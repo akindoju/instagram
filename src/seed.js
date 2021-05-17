22d914bd0491b68a989759125699fc3da9b53b03
Akndju <davidd.akindoju@gmail.com>
2021-05-17 14:24:25 +0100

    Started ig clone app, set up firebase

---
+++ src/seed.js
@@ -0,0 +1,77 @@
+/* eslint-disable no-plusplus */
+// NOTE: replace 'NvPY9M9MzFTARQ6M816YAzDJxZ72' with your Firebase auth user id (can be taken from Firebase)
+export function seedDatabase(firebase) {
+  const users = [
+    {
+      userId: 'ulAmE905O2ZjGANrTSiVgDQmh4I3',
+      username: 'Shayo',
+      fullName: 'Shayo oyahs',
+      emailAddress: 'shayo@gmail.com',
+      following: ['2'],
+      followers: ['2', '3', '4'],
+      dateCreated: Date.now(),
+    },
+
+    {
+      userId: '2',
+      username: 'Gamhnile',
+      fullName: 'George egroeg',
+      emailAddress: 'George@gmail.com',
+      following: [],
+      followers: ['ulAmE905O2ZjGANrTSiVgDQmh4I3'],
+      dateCreated: Date.now(),
+    },
+
+    {
+      userId: '3',
+      username: 'Menim',
+      fullName: 'Menim minem',
+      emailAddress: 'Menim@gmail.com',
+      following: [],
+      followers: ['ulAmE905O2ZjGANrTSiVgDQmh4I3'],
+      dateCreated: Date.now(),
+    },
+
+    {
+      userId: '4',
+      username: 'kene',
+      fullName: 'Kene enek',
+      emailAddress: 'Kene@gmail.com',
+      following: [],
+      followers: ['ulAmE905O2ZjGANrTSiVgDQmh4I3'],
+      dateCreated: Date.now(),
+    },
+  ];
+
+  // eslint-disable-next-line prefer-const
+  for (let k = 0; k < users.length; k++) {
+    firebase.firestore().collection('users').add(users[k]);
+  }
+
+  // eslint-disable-next-line prefer-const
+  for (let i = 1; i <= 5; ++i) {
+    firebase
+      .firestore()
+      .collection('photos')
+      .add({
+        photoId: i,
+        userId: '2',
+        imageSrc: `/images/users/raphael/${i}.jpg`,
+        caption: 'Saint George and the Dragon',
+        likes: [],
+        comments: [
+          {
+            displayName: 'dali',
+            comment: 'Love this place, looks like my animal farm!',
+          },
+          {
+            displayName: 'orwell',
+            comment: 'Would you mind if I used this picture?',
+          },
+        ],
+        userLatitude: '40.7128°',
+        userLongitude: '74.0060°',
+        dateCreated: Date.now(),
+      });
+  }
+}
