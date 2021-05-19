5ff0024acb921e8255c2dc4ae0d6a1905b6bf10d
Akndju <davidd.akindoju@gmail.com>
2021-05-19 09:21:29 +0100

    added styling to login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 // import { Formik } from 'formik';
 // import * as Yup from 'yup';
@@ -9,6 +10,11 @@ const Login = () => {
   const { firebase } = useContext(FirebaseContext);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const isInvalid = email === '' || password === '';
+
+  const handleLogin = () => {};
 
   useEffect(() => {
     document.title = 'Login - Instagram';
@@ -23,7 +29,47 @@ const Login = () => {
         />
       </div>
       <div className="flex flex-col w-2/5">
-        <p>I will be the form!</p>
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
+          <h1 className="flex justify-center w-full">
+            <img
+              src="/images/logo.png"
+              alt="Instagram"
+              className="mt-2 w-6/12 mb-4"
+            />
+            {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+          </h1>
+
+          <form method="POST" onSubmit={handleLogin}>
+            <input
+              type="text"
+              aria-label="Enter your email address"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2 outline-none"
+              onChange={({ target }) => setEmail(target.value)}
+            />
+
+            <input
+              type="password"
+              aria-label="Enter your password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2 outline-none"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button
+              className={`bg-blue-medium text-white w-full rounded h-8 font-bold ${
+                isInvalid && 'opacity-50'
+              }`}
+            >
+              Log In
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary rounded">
+          <p className="text-sm">Don't have an account?</p>
+          <Link to="/signup" className="font-bold text-blue-medium">
+            Sign Up
+          </Link>
+        </div>
       </div>
     </div>
   );
