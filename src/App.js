2d866b2682a020af3da2f8947b9158e3c80686c9
Akndju <davidd.akindoju@gmail.com>
2021-05-31 11:51:03 +0100

    Added routes authentication

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import useAuthListener from './hooks/use-auth-listener';
 import UserContext from './context/user';
+import ProtectedRoutes from './helpers/ProtectedRoute';
+import IsUserLoggedIn from './helpers/IsUserLoggedIn';
+const Profile = lazy(() => import('./pages/profile'));
 const Login = lazy(() => import('./pages/login'));
 const Dashboard = lazy(() => import('./pages/dashboard'));
 const SignUp = lazy(() => import('./pages/signup'));
@@ -16,9 +19,33 @@ function App() {
       <Router>
         <Suspense fallback={<p>Loading...</p>}>
           <Switch>
-            <Route path={ROUTES.LOGIN} component={Login} />
-            <Route path={ROUTES.SIGN_UP} component={SignUp} />
-            <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+            <IsUserLoggedIn
+              path={ROUTES.LOGIN}
+              loggedInPath={ROUTES.DASHBOARD}
+              user={user}
+            >
+              <Login />
+            </IsUserLoggedIn>
+
+            <IsUserLoggedIn
+              path={ROUTES.SIGN_UP}
+              loggedInPath={ROUTES.DASHBOARD}
+              user={user}
+            >
+              <SignUp />
+            </IsUserLoggedIn>
+
+            <IsUserLoggedIn
+              path={ROUTES.PROFILE}
+              loggedInPath={ROUTES.DASHBOARD}
+              user={user}
+            >
+              <Profile />
+            </IsUserLoggedIn>
+
+            <ProtectedRoutes path={ROUTES.DASHBOARD} exact user={user}>
+              <Dashboard />
+            </ProtectedRoutes>
             <Route component={NotFound} />
           </Switch>
         </Suspense>
---
+++ src/helpers/ProtectedRoute.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import { Redirect, Route } from 'react-router';
+import * as ROUTES from '../constants/routes';
+
+const ProtectedRoutes = ({ user, children, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => {
+        if (user) {
+          return children;
+        }
+
+        if (!user) {
+          return (
+            <Redirect
+              to={{
+                pathname: ROUTES.LOGIN,
+                state: { from: location },
+              }}
+            />
+          );
+        }
+
+        return null;
+      }}
+    />
+  );
+};
+
+export default ProtectedRoutes;
+
+ProtectedRoutes.propTypes = {
+  user: PropTypes.object,
+  children: PropTypes.object.isRequired,
+};
