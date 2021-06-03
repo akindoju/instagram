7de0f848b1a855ceeb1d0c700957f3e3066408ed
Akndju <davidd.akindoju@gmail.com>
2021-06-03 11:16:18 +0100

    Fixed not found page styling

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -9,8 +9,8 @@ const NotFound = () => {
   return (
     <div className="bg-gray-background">
       <Header />
-      <div className="mx-auth max-w-screen-lg">
-        <p className="text-center text 2xl">Not Found!</p>
+      <div className="mx-auto max-w-screen-lg">
+        <p className="text-center text-3xl ">Not Found!</p>
       </div>
     </div>
   );
